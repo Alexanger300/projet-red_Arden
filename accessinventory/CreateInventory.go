4059270c8ac7064707dbc197e943cc83d7eba8a6
Alexanger300 <alexandrefatoux007@gmail.com>
2025-09-15 12:05:11 +0200

    Inventory

--- accessinventory/CreateInventory.go
+++ accessinventory/CreateInventory.go
@@ -6,7 +6,7 @@ import (
 	"os"
 )
 
-func ShowInventory() {
+func CreateInventory() { // Creer un inventaire vide
 	Inventory := make(map[string]interface{})
 
 	jsonData, err := json.MarshalIndent(Inventory, "", "  ")
@@ -24,10 +24,73 @@ func ShowInventory() {
 	_, err = file.Write(jsonData)
 	if err != nil {
 		fmt.Println("Erreur:", err)
+	}
+	return
+}
+
+func removeItems(itemName string, quantity int) { //Enlever un item de l'inventaire
+	file, err := os.ReadFile("Inventory.json")
+	if err != nil {
+		fmt.Println("Erreur:", err)
 		return
+	}
+
+	Inventory := map[string]int{}
+	err = json.Unmarshal(file, &Inventory)
+	if err != nil {
+		fmt.Println("Erreur:", err)
+		return
+	}
+
+	Inventory[itemName] -= quantity // Enlever la quantite specifiee de l'item
+
+	updatedData, err := json.MarshalIndent(Inventory, "", "  ")
+	if err != nil {
+		fmt.Println("Erreur:", err)
+		return
+	}
+
+	err = os.WriteFile("Inventory.json", updatedData, 0644)
+	if err != nil {
+		fmt.Println("Erreur:", err)
+		return
+	}
+	for item, qty := range Inventory {
+		fmt.Println(item, ":", qty)
 	}
 }
 
+func addItems(itemName string, quantity int) { //Ajouter un item a l'inventaire
+	file, err := os.ReadFile("Inventory.json")
+	if err != nil {
+		fmt.Println("Erreur:", err)
+		return
+	}
+
+	Inventory := map[string]int{}
+	err = json.Unmarshal(file, &Inventory)
+	if err != nil {
+		fmt.Println("Erreur:", err)
+		return
+	}
+
+	Inventory[itemName] += quantity // Ajouter la quantite a l'item
+
+	updatedData, err := json.MarshalIndent(Inventory, "", "  ")
+	if err != nil {
+		fmt.Println("Erreur:", err)
+		return
+	}
+
+	err = os.WriteFile("Inventory.json", updatedData, 0644)
+	if err != nil {
+		fmt.Println("Erreur:", err)
+		return
+	}
+	for item, qty := range Inventory {
+		fmt.Println(item, ":", qty)
+	}
+}
 func main() {
-	ShowInventory()
+	removeItems("Potion de soin", 3)
 }
